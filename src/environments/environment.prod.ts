ebe3db7915558f11cc9ae8001e32125a3047f6ab
wasuwat-kubeops <wasuwat.p@kubeops.guru>
2022-04-04 15:18:51 +0700

    Update: environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  someSecret: "develop-secret"
+  someSecret: "#{SOME_SECRET}#"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  someSecret: "develop-secret"
+  someSecret: "#{SOME_SECRET}#"
 };
 
 /*
